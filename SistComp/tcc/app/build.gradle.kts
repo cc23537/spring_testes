b1eb52ecb7a14481d5011858672bba925efe2e4c
cc23632 <cc23632@g.unicamp.br>
2024-12-06 23:40:03 -0300

    a

--- SistComp/tcc/app/build.gradle.kts
+++ SistComp/tcc/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation ("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation ("com.prolificinteractive:material-calendarview:1.4.3")
 
 
 }
